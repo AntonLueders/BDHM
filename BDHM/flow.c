27c1d6eeae4e3d2c8f6ddd78d012cfc04ec1e447
AntonLueders <145451222+AntonLueders@users.noreply.github.com>
2024-03-05 22:30:05 +0100

    Update flow.c

--- BDHM/flow.c
+++ BDHM/flow.c
@@ -10,7 +10,8 @@ void PoiseuilleFlow(Particle *p_i, double v_flow[3]) {
         v_flow[d] = 0.;
     }
     v_flow[0] = v_flow_max * (1. - pow(p_i->r[2] - Box[2] / 2., 2.) / pow(Box[2] / 2., 2.));
-    
+
+    // The flow field can only result in positive velocities
     if (v_flow[0] < 0.) {
         printf("ERROR: Stability problem. (Particle behind wall)\n");
         exit(1);
